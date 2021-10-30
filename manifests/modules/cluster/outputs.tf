30a818d0761febc18928f55baea29b9c54f7baba
doconnor <d1@outlook.ie>
2021-10-30 16:43:52 +0100

    rm sensitive outputs by default

--- manifests/modules/cluster/outputs.tf
+++ manifests/modules/cluster/outputs.tf
@@ -1,16 +1,16 @@
-output "kubeconfig" {
-  value     = ["$({linode_lke_cluster.cluster.kubeconfig}"]
-  sensitive = true
-}
-output "api-endpoints" {
-  value = ["$({linode_lke_cluster.cluster.api_endpoints}"]
-}
 output "status" {
   value = ["$({linode_lke_cluster.cluster.status}"]
 }
-output "id" {
-  value = ["$({linode_lke_cluster.cluster.id}"]
-}
-output "pool" {
-  value = ["$({linode_lke_cluster.cluster.pool}"]
-}
+# output "kubeconfig" {
+#   value     = ["$({linode_lke_cluster.cluster.kubeconfig}"]
+#   sensitive = true
+# }
+# output "api-endpoints" {
+#   value = ["$({linode_lke_cluster.cluster.api_endpoints}"]
+# }
+# output "id" {
+#   value = ["$({linode_lke_cluster.cluster.id}"]
+# }
+# output "pool" {
+#   value = ["$({linode_lke_cluster.cluster.pool}"]
+# }
